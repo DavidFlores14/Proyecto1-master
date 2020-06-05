4d6f1a3064133befb06300c11a80d6b8768bf221
David Flores Águila <david.flores17@tectijuana.edu.mx>
2020-06-05 13:13:46 -0700

    Initial commit

---
+++ Proyecto1/Migrations/202006052010110_Primera.cs
@@ -0,0 +1,16 @@
+namespace Proyecto1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Primera : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
